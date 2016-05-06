5e715cdb42d1798ec8da3d2c0fd902b2e5c05475
Markus <markus.kreth@web.de>
2016-05-06 14:25:26 +0200

    Test with mocked Graphics for Column Painting.

--- src/test/java/de/kreth/formatter/table/ColumnPaintTest.java
+++ src/test/java/de/kreth/formatter/table/ColumnPaintTest.java
@@ -1,7 +1,9 @@
 package de.kreth.formatter.table;
 
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.*;
 
+import java.awt.FontMetrics;
 import java.awt.Graphics;
 import java.awt.Rectangle;
 
@@ -9,10 +11,13 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import de.kreth.formatter.table.Column.VerticalAlign;
+
 
 public class ColumnPaintTest {
 
    private Graphics g;
+   private FontMetrics metrics;
    
    @BeforeClass
    public static void setUpBeforeClass() throws Exception {}
@@ -20,13 +25,41 @@ public class ColumnPaintTest {
    @Before
    public void setUp() throws Exception {
       g = mock(Graphics.class);
+      metrics = mock(FontMetrics.class);
+      when(g.getFontMetrics()).thenReturn(metrics);
    }
 
    @Test
-   public void test() {
+   public void testPaintLeftText() {
+	  when(metrics.stringWidth(anyString())).thenReturn(12);
       Rectangle bounds = new Rectangle(0, 0, 20, 10);
-//      Column col = new Column();
-//      col.paint(g, bounds);
+      Column col = new Column.Builder().setColumnName("Column 1").setVerticalAlign(VerticalAlign.LEFT).setColumnWidth(12).build();
+      
+      col.paint(g, bounds, "Content 1");
+      verify(g).clipRect(0, 0, 20, 10);
+      verify(g).drawString("Content 1", 0, 0);
+   }
+
+   @Test
+   public void testPaintRightText() {
+	  when(metrics.stringWidth(anyString())).thenReturn(12);
+      Rectangle bounds = new Rectangle(0, 0, 20, 10);
+      Column col = new Column.Builder().setColumnName("Column 1").setVerticalAlign(VerticalAlign.RIGHT).setColumnWidth(12).build();
+      
+      col.paint(g, bounds, "Content 1");
+      verify(g).clipRect(0, 0, 20, 10);
+      verify(g).drawString("Content 1", 8, 0);
+   }
+
+   @Test
+   public void testPaintCenterText() {
+	  when(metrics.stringWidth(anyString())).thenReturn(12);
+      Rectangle bounds = new Rectangle(0, 0, 20, 10);
+      Column col = new Column.Builder().setColumnName("Column 1").setVerticalAlign(VerticalAlign.CENTER).setColumnWidth(12).build();
+      
+      col.paint(g, bounds, "Content 1");
+      verify(g).clipRect(0, 0, 20, 10);
+      verify(g).drawString("Content 1", 4, 0);
    }
 
 }
